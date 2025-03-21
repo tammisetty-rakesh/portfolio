88d1dcf316a2a73395dbf0bf7c7004d10405ff0a
tammisetty-rakesh <tammisettyrakesh@gmail.com>
2025-03-21 13:41:57 +0530

    Css fix

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    emptyOutDir: false,
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react(), tailwindcss()],
+  base: "/pedrotech-portfolio",
 });
